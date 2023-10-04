c443336eae28ce63508fd0f2ef958cff60d3d330
akhanina <146199953+akhanina@users.noreply.github.com>
2023-10-04 14:40:02 +0300

    cycles.js

--- cycles.js
+++ cycles.js
@@ -1,13 +1,13 @@
 /*
 //144
-let arr = [1, 2, 3, 4, 5, 6]
+let arr = ['a', 'b', 'c', 'd', 'e']
 
 for(let elem of arr){
     console.log(elem)
 }
 
 //145
-let obj={a:1, b:2, c:3}
+let obj={x:1, y:2, z:3}
 
 for(let key in obj){
     console.log(key)
